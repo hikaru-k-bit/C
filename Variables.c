539440d11a934f1e872157f303dbf5b79cc7498f
Nemirphen <hikaru_dano@vk.com>
2024-04-30 01:39:59 +0300

    Работа с переменными

---
+++ Variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int age = 21;
+    float gpa = 2.05;
+    char grade = 'C';
+    char name[] = "Nemirphen";
+
+    printf("Your name is %s\n", name);
+    printf("You are %d years old\n", age);
+    printf("Your average grade is %c\n", grade);
+    printf("Your GPA is %f", gpa);
+
+    return 0;
+}
